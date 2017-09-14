614d02f7dd420e5af654b9786a1353e2b4908bd9
msk1145 <rail_moon@naver.com>
2017-09-14 11:42:23 +0900

    d

--- app/src/main/java/edu/android/and57_git/MainActivity.java
+++ app/src/main/java/edu/android/and57_git/MainActivity.java
@@ -6,11 +6,13 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tv = (TextView) findViewById(R.id.textView);
+        tv = (TextView) findViewById(R.id.textView);
     }
 
     public void test() {
